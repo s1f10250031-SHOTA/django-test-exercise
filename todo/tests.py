796f8a495f18780a6f0d7261df8de97fa3021ebe
1F10250187 <s1f102501877@iniad.org>
2026-07-09 14:05:52 +0900

    終了機能のテスト追加

--- todo/tests.py
+++ todo/tests.py
@@ -150,3 +150,21 @@ class TodoViewTestCase(TestCase):
         response = client.post('/1/delete')
 
         self.assertEqual(response.status_code, 404)
+
+    def test_close_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        
+        client = Client()
+        response = client.get('/{}/close'.format(task.pk))
+
+        self.assertEqual(response.status_code, 302)
+        
+        updated_task = Task.objects.get(pk=task.pk)
+        self.assertTrue(updated_task.completed)
+
+    def test_close_fail(self):
+        client = Client()
+        response = client.get('/1/close')
+
+        self.assertEqual(response.status_code, 404)
